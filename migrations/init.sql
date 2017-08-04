fb6b9d02b02bc79db91ccff40d64695f8381d3b3
SimonIsMe <szym1991@gmail.com>
2017-08-04 18:42:15 +0200

    Create login_session table in init.sql migration

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,2 +1,4 @@
 CREATE TABLE login_password (login varchar(255) NOT NULL, password VARCHAR(255) NOT NULL);
 CREATE UNIQUE INDEX login_password_unique_index ON login_password (login);
+
+CREATE TABLE login_session (login varchar(255) NOT NULL, session_id VARCHAR(255) NOT NULL);
